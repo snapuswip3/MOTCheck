5a9976c404f758886e36a42fbb86426fca732e87
Dave Rose <snapuswip3@gmail.com>
2021-10-04 22:11:01 +0100

    Add project files.

---
+++ MOTCheck/App_Code/Controller/GovUKServiceAdaptor.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace MOTCheck.Controller
+{
+    public static class GovUKServiceAdaptor
+    {
+        private static readonly HttpClient _httpClient;
+
+        static GovUKServiceAdaptor()
+        {
+            _httpClient = new HttpClient();
+            _httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json+v6"));
+            //TODO AppSettings / secrets?
+            _httpClient.DefaultRequestHeaders.Add("x-api-key", "fZi8YcjrZN1cGkQeZP7Uaa4rTxua8HovaswPuIno");
+            //_httpClient.DefaultRequestHeaders.CacheControl = new CacheControlHeaderValue()
+            //{ 
+            //    NoCache = true,
+            //    NoStore = true
+            //};
+            _httpClient.Timeout = new TimeSpan(hours: 0, minutes: 0, seconds: 10);
+            //https://dvsa.github.io/mot-history-api-documentation/
+        }
+
+        public static bool GetMOTTests(string a_sRegistration, out string a_sResponseContent)
+        {
+            bool bOK = false;
+
+            string sEndPoint = "https://beta.check-mot.service.gov.uk/trade/vehicles/mot-tests?registration=" + a_sRegistration;
+
+            HttpResponseMessage httpResponseMessage = _httpClient.GetAsync(sEndPoint).ConfigureAwait(false).GetAwaiter().GetResult();
+            a_sResponseContent = httpResponseMessage.Content.ReadAsStringAsync().ConfigureAwait(false).GetAwaiter().GetResult();
+
+            if (httpResponseMessage.StatusCode == HttpStatusCode.OK) bOK = true;
+
+            return bOK;
+        }
+    }
+}
---
+++ MOTCheck/Index.aspx.cs
@@ -0,0 +1,12 @@
+﻿using MOTCheck.Controller;
+using System;
+
+public partial class Index : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        string sTest;
+        GovUKServiceAdaptor.GetMOTTests("AM03FXJ", out sTest);
+        TestLabel.Text = sTest;
+    }
+}
